c5783ae31c987d6b434507823af438513bae45c8
CarloSalaris <carloa.salaris@gmail.com>
2023-07-12 13:39:51 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main-layout')
+
+@section('content')
+    <h1>Home</h1>
+
+
+    <h2>film</h2>
+@endsection
+
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Guest\PageController as PageController;
+
+Route::get('/', [PageController :: class, 'index']);
+
